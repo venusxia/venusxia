860f55f0c8b9d2c45de1da62494bc888c5ffa60a
Venus_Xia <137294149+venusxia@users.noreply.github.com>
2023-06-21 19:47:46 +0700

    Create README.php

---
+++ README.php
@@ -0,0 +1,4 @@
+<?php
+  $bio = "👋 Hi there! I’m a student with a passion for the world of IT. I love learning new things and exploring the latest technology 👨‍💻. #ITenthusiast #lifelonglearner";
+  echo $bio;
+?>
